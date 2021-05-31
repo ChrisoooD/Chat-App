e8b300ab284bd82086539a2fc104e0fece7cc870
Edward-jiang495 <ejiang@smu.edu>
2021-05-31 12:29:25 -0400

    logout finished

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { ChatEngine } from 'react-chat-engine';
+import { useHistory } from "react-router";
 import ChatFeed from "./ChatFeed";
 const Chat = () => {
 
+    let history = useHistory();
+
+    const handleLogOut = ()=>{
+        localStorage.clear();
+        history.push('/signin');
+    }
+    
     return (
-         <ChatEngine
+        <div>
+            <div className='nav-bar'>
+                <div className ='logo-tab'>
+                    Chatting Software
+                </div>
+                <div onClick= {handleLogOut}className="logout-tab">
+                    Log out 
+                </div>
+            </div>
+        <ChatEngine
             height = "100vh"
             projectID= "1501218f-e751-4570-8896-2549d6c65eaf"
             userName={localStorage.getItem('username')}
             userPassword={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
         />
+
+        </div>
+         
     );
     
 }
